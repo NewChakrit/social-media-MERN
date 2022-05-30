fe5f2bd12435152879d37e9503ab0f96041355bc
NewChakrit <imnew108@gmail.com>
2022-05-30 02:33:24 -0700

    'posts'

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import "./Post.css";
+import Comment from "../../img/comment.png";
+import Share from "../../img/share.png";
+import Heart from "../../img/like.png";
+import Notlike from "../../img/notlike.png";
+
+const Post = ({ data, id }) => {
+  return (
+    <div className="post">
+      <img src={data.img} alt={id} />
+
+      <div className="postReact">
+        <img src={data.liked ? Heart : Notlike} alt="" />
+        <img src={Comment} alt={id} />
+        <img src={Share} alt={id} />
+      </div>
+
+      <span>{data.likes} likes</span>
+
+      <div className="detail">
+        <span>
+          <b>{data.name}</b>
+        </span>
+        <span> {data.desc}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
---
+++ src/components/posts/Posts.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Posts.css";
+import { PostsData } from "../../data/PostsData";
+import Post from ".././post/Post";
+
+const Posts = () => {
+  return (
+    <div className="posts">
+      {PostsData.map((post, id) => {
+        return <Post data={post} id={id} key={id} />;
+      })}
+    </div>
+  );
+};
+
+export default Posts;
