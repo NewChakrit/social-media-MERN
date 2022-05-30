b4b7a617e6955716a797000d0386e6f5feff9c65
NewChakrit <imnew108@gmail.com>
2022-05-30 01:35:53 -0700

    'post-share'

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import PostSide from "../../components/postSide/PostSide";
 import ProfileSide from "../../components/profileSide/ProfileSide";
 import "./Home.css";
 const Home = () => {
   return (
     <div className="home">
       <ProfileSide />
-      <div className="postSide">Posts</div>
+      <PostSide />
       <div className="RightSide">RightSide</div>
     </div>
   );
