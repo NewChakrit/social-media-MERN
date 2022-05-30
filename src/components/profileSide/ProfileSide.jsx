a0c8980aea8b8e836f806507b82bceeaedef6b73
NewChakrit <imnew108@gmail.com>
2022-05-29 22:32:19 -0700

    'setup-components-profileCard'

---
+++ src/components/profileSide/ProfileSide.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Logosearch from "../logoSearch/Logosearch";
+import "./ProfileSide.css";
+
+const ProfileSide = () => {
+  return (
+    <div className="profileSide">
+      <Logosearch />
+    </div>
+  );
+};
+
+export default ProfileSide;
